ed7b4632772757bfe76977c2789ccefbe5d7397a
Dinukacyber <151698236+Dinukacyber@users.noreply.github.com>
2024-05-09 18:30:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 process.env.NODE_OPTIONS = '--max_old_space_size=2560'//2.5
 const DB_URL =  process.env.DATABASE_URL || '';
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //inrl~63d0cfG7e7b703aba6f32d0b45f043a0917e
+    SESSION_ID: process.env.SESSION_ID || 'inrl~63d0cfG7e7b703aba6f32d0b45f043a0917e', //inrl~63d0cfG7e7b703aba6f32d0b45f043a0917e
         API_KEY: process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME
     },
